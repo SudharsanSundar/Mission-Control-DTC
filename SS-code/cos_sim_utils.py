83a3a9b1a118d4fe09ead185097ee39ad1f0374e
Salman Abdullah <51970153+salman-abdullah@users.noreply.github.com>
2024-02-23 01:31:06 -0800

    added faiss index

--- SS-code/cos_sim_utils.py
+++ SS-code/cos_sim_utils.py
@@ -1,1 +1,21 @@
 # TODO: Fill out!
+
+import faiss
+
+class FaissIndex():
+    def __init__(self, model, embeddings, k=5):
+        self.embeddings = np.array(embeddings)
+        self.index = None
+        self.k = k
+        self.model = model
+
+
+    def build(self):
+        d = self.embeddings.shape[1]
+		self.index = faiss.IndexFlatIP(d)
+		self.index.add(self.embeddings)
+
+    def search(self, text):
+        embedding = self.model.encode(text)
+        distances, indices = self.index.search(embedding, k=self.k)
+        return distances, indices
